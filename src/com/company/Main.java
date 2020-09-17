4176e0641492fe0df89a324ed9615653e3701564
omurzak2020 <omurzak825@gmail.com>
2020-09-17 12:27:23 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int temperature = 27;
+
+        if (temperature >= 15) {
+            System.out.println(" можно идти гулять");
+            System.out.println("End of if");
+        }
+        if (temperature == 25)
+    }
+}
